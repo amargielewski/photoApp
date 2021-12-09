82a424d9a0c512af9ccf9bf7339e95070dd9ce5a
amargielewski <aleksander.margielewski4@gmail.com>
2021-12-09 12:29:31 +0100

    Create page to 320px

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,41 +1,20 @@
 import PhotoList from "../../components/photoList/PhotoList";
 import { useCollection } from "../../hooks/useCollection";
-import { useEffect, useState } from "react";
-import { ChevronsUp } from "react-feather";
+import styled from "styled-components";
+import { useState } from "react";
+
 import {
+  StyledInfoBox,
   StyledInput,
   StyledSearchContainer,
   StyledInputText,
   StyledWrapper,
-  StyledInfoBox,
   StyledScrollButton,
 } from "./HomeStyle";
 
 function Home() {
-  const { documents, error, isPending } = useCollection("photos");
+  const { documents, error } = useCollection("photos");
   const [name, setName] = useState("");
-  const [isVisible, setIsVisible] = useState(false);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
-  useEffect(() => {
-    const toggleVisibility = () => {
-      if (window.pageYOffset > 400) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
-
-    window.addEventListener("scroll", toggleVisibility);
-
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
 
   let project =
     documents &&
@@ -50,7 +29,6 @@ function Home() {
       return false;
     });
 
-  // console.log("isPending:", isPending);
   return (
     <StyledWrapper>
       <StyledSearchContainer>
@@ -59,21 +37,13 @@ function Home() {
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          placeholder="Username"
         />
       </StyledSearchContainer>
 
       {error && <StyledInfoBox>{error}</StyledInfoBox>}
-      {isPending && <StyledInfoBox>Loading...</StyledInfoBox>}
-      {project && <PhotoList document={project} />}
+      {documents && <PhotoList document={project} />}
       {project && project.length === 0 && (
-        <StyledInfoBox>This user has no photos :(</StyledInfoBox>
-      )}
-      {isVisible && (
-        <StyledScrollButton onClick={scrollToTop}>
-          {" "}
-          <ChevronsUp size={25} color={"#fff"} />
-        </StyledScrollButton>
+        <StyledInfoBox>This user has no photos :C</StyledInfoBox>
       )}
     </StyledWrapper>
   );
